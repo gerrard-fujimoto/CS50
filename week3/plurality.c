69665884daec22c87413463e713971609a1fa559
gerrard-fujimoto <kosuke.fujimoto@claves.co.jp>
2025-11-10 05:50:41 +0000

    Mon, Nov 10, 2025, 2:50 PM +09:00

--- week3/plurality.c
+++ week3/plurality.c
@@ -66,6 +66,10 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(ca))
+    }
     return false;
 }
 
