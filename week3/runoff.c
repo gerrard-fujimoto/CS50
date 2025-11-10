709b37e4866fb8fe53de367e187333d6cc5d9d0e
gerrard-fujimoto <kosuke.fujimoto@claves.co.jp>
2025-11-10 04:32:09 +0000

    Mon, Nov 10, 2025, 1:32 PM +09:00

--- week3/runoff.c
+++ week3/runoff.c
@@ -145,7 +145,6 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    int majority
     for (int i = 0; i < voter_count; i++)
     {
         for (int j = 0;  j < candidate_count; j++)
